8c6481369f25fc9eb521cb16acce9bf9df5d649c
Alan Leonard <leonardalan7@gmail.com>
2016-07-24 10:46:00 +0100

    before street view

--- includes/registration.php
+++ includes/registration.php
@@ -7,7 +7,7 @@ $lname = $request->lname;
 $email = $request->email;
 $password = $request->password;
 $age = $request->age;
-$parent = $request->parents;
+$parent = $request->parent;
 $mobilenumber = $request->mobilenumber;
 $altemail = $request->altemail;
 $add1 = $request->add1;
@@ -18,7 +18,7 @@ $add2 = $request->add2;
        // $query = mysqli_query($link,"UPDATE customers SET latitude = $lat WHERE email = $email ");
 
 	//MySqli Update Query
-$results = $mysqli->query("INSERT INTO customers (fname, lname, age, parent, password,email,mobileNumber,add1,add2,signup,alt_email)
+$results = $mysqli->query("INSERT INTO customers (fname, lname, Age, parent, password,email,mobileNumber,add1,add2,signup,alt_email)
 VALUES ('$fname','$lname',$age,'$parent','$password','$email','$mobilenumber','$add1','$add2',now(),'$altemail')");
 
 
